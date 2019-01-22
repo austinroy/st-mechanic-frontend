630f2690400020cc31b76b0d4392be3d549f6105
Austin Roy <austin.roy@andela.com>
2019-01-22 20:17:12 +0300

    File cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 // import logo from './logo.svg'
-import './App.css'
 import './sass/main.scss'
 import { Layout } from 'antd'
 import SiteHeader from './components/Header/SiteHeader'
