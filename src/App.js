0f2c6acffc516af7fd2eba849aa8062ac169143e
Austin Roy <austin.roy@andela.com>
2019-01-22 18:40:50 +0300

    Fix SASS setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 // import logo from './logo.svg'
 import './App.css'
-import './sass/main.css'
+import './sass/main.scss'
 import { Layout } from 'antd'
 import SiteHeader from './components/Header/SiteHeader'
-import SideMenu from './components/SideMenu/SideMenu'
-import Segment from './components/Segment/Segment'
 class App extends Component {
   render() {
     return (
