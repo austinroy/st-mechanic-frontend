9ec402a8f83fdee86d72f48bd93512fb14636211
Austin Roy <austin.roy@andela.com>
2019-01-31 22:00:02 +0300

    Fix typo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 import './sass/main.scss'
 import { Layout } from 'antd'
 import SiteFooter from './components/Footer/SiteFooter';
+import SiteHeader from './components/Header/SiteHeader'
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SiteHeader />
         <Layout>{this.props.children}</Layout>
         <Layout>
           <SiteFooter />
