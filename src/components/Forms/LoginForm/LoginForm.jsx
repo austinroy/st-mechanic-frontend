e096fe20390e9fbc39ddb2918de07c5e55e8a7ac
Austin Roy <austin.roy@andela.com>
2019-02-03 18:31:56 +0300

    Style Login Form and import logos

--- src/components/Forms/LoginForm/LoginForm.jsx
+++ src/components/Forms/LoginForm/LoginForm.jsx
@@ -10,11 +10,16 @@ class LoginForm extends Component {
           <Input
             prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
             placeholder="Username"
+            className="u-margin-bottom-small"
           />
           <Input.Password
             prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
             placeholder="Password"
+            className="u-margin-bottom-small"
           />
+          <Button className="u-margin-bottom-small">Login</Button>
+          <br />
+          No account? Sign Up <a href="/signup">Here</a>
         </form>
       </div>
     )
