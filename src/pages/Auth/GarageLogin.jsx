95e395270ce0eada7c59b2e12fbe72ea81cac627
Austin Roy <austin.roy@andela.com>
2019-02-17 16:59:02 +0300

    Refactor garage auth

--- src/pages/Auth/GarageLogin.jsx
+++ src/pages/Auth/GarageLogin.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import { Input, Button, Icon } from 'antd'
+import Banner from '../../assets/img/Relic-55.png'
+import Logo from '../../assets/logos/Logo-Black-&-Yellow.png'
 
 class GarageLogin extends Component {
   render() {
     return (
       <div className="garage-login">
+       <div className="signup-image">
+          <img src={Banner} alt="login-image" className="img-login" />
+        </div>
         <div className="login-form">
-          <h2>Garage Log In</h2>
-          <form>
+          <img src={Logo} alt="logo" className="login-logo" />
+          <h2 className="login-header">Welcome Back!</h2>
+          <h3 className="login-subheader">Sign in to Continue</h3>
+          <form className="u-margin-top-medium">
             <Input
               prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
               placeholder="Username"
@@ -18,9 +25,16 @@ class GarageLogin extends Component {
               placeholder="Password"
               className="u-margin-bottom-small"
             />
-            <Button className="u-margin-bottom-small">Login</Button>
+            <Button type="primary" className="u-margin-bottom-small">
+              Login
+            </Button>
             <br />
-            No account? Sign Up <a href="/garage-signup">Here</a>
+            No account? Sign Up{' '}
+            <a href="/garage-signup" className="signup-link">
+              Here
+            </a>
+            <br/>
+            <a href="/">Forgot password</a>
           </form>
         </div>
       </div>
--- src/pages/Auth/GarageSignUp.jsx
+++ src/pages/Auth/GarageSignUp.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import { Input, Button, Icon } from 'antd'
+import Banner from '../../assets/img/standard-street-view 2.png'
+import Logo from '../../assets/logos/Logo-Black-&-Yellow.png'
 
 class GarageSignUp extends Component {
   render() {
     return (
-      <div className="garage-login">
+      <div className="garage-signup">
+        <div className="signup-image">
+          <img src={Banner} alt="signup-image" className="img-signup" />
+        </div>
         <div className="signup-form">
-          <h2>Sign Up Your Garage</h2>
-          <form>
+          <img src={Logo} alt="logo" className="signup-logo" />
+          <br />
+          <span className="signup-header">Try us out!</span>
+          <br />
+          <span className="signup-subheader">
+            Sign Up Your Garage to get started
+          </span>
+          <form className="u-margin-top-medium">
             <Input
               placeholder="Garage Name"
               className="u-margin-bottom-small"
@@ -28,7 +39,14 @@ class GarageSignUp extends Component {
               className="u-margin-bottom-small"
             />
             <br />
-            <Button className="u-margin-bottom-small">Submit</Button>
+            <Button type="primary" className="u-margin-bottom-small">
+              Sign Up
+            </Button>
+            <br />
+            Already have an account?{' '}
+            <a href="/garage-login" className="login-link">
+              Login
+            </a>
           </form>
         </div>
       </div>
