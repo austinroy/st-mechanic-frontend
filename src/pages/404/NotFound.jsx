95d5bf17e53ba2dd72adc6cfddbc211cd88ce637
Austin Roy <austin.roy@andela.com>
2019-03-13 16:05:32 +0300

    cleanup

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Exception from 'ant-design-pro/lib/Exception'
 import 'ant-design-pro/dist/ant-design-pro.css'
 
-const NotFound = () => <Exception type="404" desc="Page Not Found" />
+const NotFound = () => (
+  <Exception style={{ minHeight: '100vh' }} type="404" desc="Page Not Found" />
+)
 
 export default NotFound
