b0202e39766a334d744a7a1f12f322c3412f37ef
Austin Roy <austin.roy@andela.com>
2019-04-02 21:53:44 +0300

    Center Tabs

--- src/pages/GarageDashboard/GarageDashboard.jsx
+++ src/pages/GarageDashboard/GarageDashboard.jsx
@@ -1,36 +1,29 @@
 import React, { Component } from 'react'
 import SideMenu from './SideMenu'
 import { Route, Switch } from 'react-router-dom'
-import { Layout } from 'antd'
+import { Tabs } from 'antd'
 import JobsManagement from './Jobs/JobsManagement'
+import TopBar from './TopBar'
 
-const { Content, Sider } = Layout
+const { TabPane } = Tabs
 
 class GarageDashboard extends Component {
   render() {
     return (
       <div className="garage-dashboard">
-        <Layout>
-          <Sider>
-            <SideMenu />
-          </Sider>
-          <Content style={{ position: 'relative' }}>
-            <Switch>
-              <Route exact path="/garagedashboard/parts" />
-              <Route exact path="/garagedashboard/mechanics" />
-              <Route exact path="/garagedashboard/payments" />
-              <Route
-                exact
-                path="/garagedashboard/jobs"
-                render={() => <JobsManagement />}
-              />
-              <Route exact path="/garagedashboard/jobs/bookings" />
-              <Route exact path="/garagedashboard/active" />
-              <Route exact path="/garagedashboard/completed" />
-              <Route exact path="/garagedashboard/disputed" />
-            </Switch>
-          </Content>
-        </Layout>
+        <TopBar />
+        <Tabs defaultActiveKey="1" tabBarStyle={{ textAlign: 'center' }}>
+          <TabPane tab="Tab 1" key="1">
+            Content of Tab Pane 1
+          </TabPane>
+          <TabPane tab="Tab 2" key="2">
+            Content of Tab Pane 2
+          </TabPane>
+          <TabPane tab="Tab 3" key="3">
+            Content of Tab Pane 3
+          </TabPane>
+        </Tabs>
+        ,
       </div>
     )
   }
