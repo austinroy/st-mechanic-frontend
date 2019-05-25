d7d9ae65de72a34f6e812f386022205ccc87d497
Austin Roy <austin.roy@andela.com>
2019-05-25 20:59:36 +0300

    Change Homepage Header

--- src/pages/home/Header.jsx
+++ src/pages/home/Header.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
+import Logo from '../../assets/logos/Logo-White-&-Yellow.png'
 
 const Header = () => {
   return (
     <div className="header-home">
-      <div className="header-content">
-        <div className="header-slogan">
-          <span className="slogan-top">We bring</span>
-          <br />
-          <span className="slogan-middle">
-            TRUST, TRASPARENCY & CONVENIENCE
-          </span>
-          <br />
-          <span className="slogan-bottom">To your repair experience</span>
+      <div className="header-nav">
+        <div>
+          <img src={Logo} alt="logo" className="home-logo" />
         </div>
-        <button className="btn-yellow">Get Your Quotes</button>
+        <div className="home-links u-margin-top-medium">
+          <a href="#" className="home-link--regular">
+            ABOUT US
+          </a>
+          <a href="#" className="home-link--regular">
+            HOW IT WORKS
+          </a>
+          <a href="#" className="home-link--regular">
+            FOR GARAGES
+          </a>
+          <a href="#" className="home-link--btn-inverted">
+            LOGIN
+          </a>
+          <a href="#" className="home-link--btn">
+            SIGN UP
+          </a>
+        </div>
+      </div>
+      <div className="intro u-margin-bottom-big">
+        <p className="intro-text">
+          Choose <br /> the best <span className="underline-bottom">hands</span>
+          <br /> for your car
+        </p>
+        <p className="intro-sub">
+          One stop shopping for all your automotive needs
+          <br />
+          Join us today
+        </p>
       </div>
     </div>
   )
